5a824dfd3eb6aedf22b19e70959e7780d865ba01
cleetus_pixel <joshuakim5477@gmail.com>
2025-09-17 18:59:23 -0400

    so much work done?

---
+++ Assets/FirstPersonMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // Get the Rigidbody component
+    }
+
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = (transform.right * moveX + transform.forward * moveZ).normalized;
+    }
+
+    void MovePlayer()
+    {
+        rb.velocity = moveDirection * moveSpeed + new Vector3(0, rb.velocity.y, 0);
+    }
+}
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,51 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour
 {
-
     public float speed = 5f;
-    public float jumpHeight = 2f;
-    public float gravity = -9.81f;
 
     private CharacterController controller;
-    private Transform cam;
-    private Vector3 velocity;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        cam = Camera.main.transform; // automatically find main camera
     }
 
     void Update()
     {
         // --- Input ---
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal"); // A/D
+        float vertical = Input.GetAxisRaw("Vertical");     // W/S
 
-        // Move relative to camera
-        Vector3 direction = cam.forward * vertical + cam.right * horizontal;
-        direction.y = 0f;
+        // Move in world space (X/Z only)
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
         if (direction.magnitude >= 0.1f)
         {
             controller.Move(direction.normalized * speed * Time.deltaTime);
         }
-
-        // --- Jumping & Gravity ---
-        if (controller.isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f; // keeps grounded
-            if (Input.GetButtonDown("Jump"))
-            {
-                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            }
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
     }
 }
