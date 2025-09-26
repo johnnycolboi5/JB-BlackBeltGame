cc3eec7f7ce00af890a5d7fbabb0f1f0a8a08afc
cleetus_pixel <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2025-09-26 18:59:25 -0400

    breerthth
    thethe

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    public MeshFilter TreeMesh;
+    public Mesh[] TreeMeshOptions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float randomAngle = Random.Range(1f, 361f);
+        transform.rotation = Quaternion.Euler(0f, randomAngle, 0f);
+        
+        if (TreeMeshOptions.Length > 0)
+        {
+            TreeMesh.mesh = TreeMeshOptions[Random.Range(0, TreeMeshOptions.Length)];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
